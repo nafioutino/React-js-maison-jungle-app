d4e3e0d6b8f9ff9bde5c2fda1d7e1c29186d4edf
nafioutino <nafiou531@gmail.com>
2024-12-19 17:16:18 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Banner from "./components/Banner";
+import Cart from "./components/Cart";
+
+function App() {
+  return (
+    <div>
+      <Banner />
+      <Cart/>
+    </div>
+
+  )
+}
+
+export default App
