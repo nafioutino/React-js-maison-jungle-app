9dc578bf96f263b171f0783710c9fbf721a269e4
nafioutino <nafiou531@gmail.com>
2024-12-23 18:06:18 +0100

    syle de app amélioré

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import Banner from "./components/Banner";
-import Cart from "./components/Cart";
-import Footer from "./components/Footer";
-import ShoppingList from "./components/ShoppingList";
-
+import Banner from './components/Banner'
+import logo from './assets/logo.png'
+import Cart from './components/Cart'
+import Footer from './components/Footer'
+import ShoppingList from './components/ShoppingList'
+import './styles/Layout.css'
 
 function App() {
-  return (
-    <div>
-      <Banner />
-      <Cart/>
-      <ShoppingList/>
-      <Footer/>
-      
-    </div>
-
-  )
+	return (
+		<div>
+			<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+			<div className='lmj-layout-inner'>
+				<Cart />
+				<ShoppingList />
+			</div>
+			<Footer />
+		</div>
+	)
 }
 
 export default App
