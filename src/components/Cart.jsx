02d07b9063ae55193040fcdd25e8b844aaf3d07c
nafioutino <nafiou531@gmail.com>
2024-12-23 17:54:18 +0100

    amélioration de la carte

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,24 +1,35 @@
-import { useState } from "react";
-
+import { useState } from 'react'
+import '../styles/Cart.css'
 
 function Cart() {
 	const monsteraPrice = 8
 	const [cart, updateCart] = useState(0)
-	const [isOpen, setIsOpen] = useState(false)
+	const [isOpen, setIsOpen] = useState(true)
 
 	return isOpen ? (
-		<div className="lmj-cart">
+		<div className='lmj-cart'>
+			<button
+				className='lmj-cart-toggle-button'
+				onClick={() => setIsOpen(false)}
+			>
+				Fermer
+			</button>
 			<h2>Panier</h2>
-			<div>
-				Monstera: {monsteraPrice}€
-				<button onClick={() => updateCart(cart + 1)}>Ajouter</button>
-			</div>
-			<h3>Total : {monsteraPrice * cart}€ </h3>
+			<div>Monstera : {monsteraPrice}€</div>
+			<button onClick={() => updateCart(cart + 1)}>Ajouter</button>
+			<h3>Total : {monsteraPrice * cart}€</h3>
+			<button onClick={() => updateCart(0)}>Vider le panier</button>
 		</div>
 	) : (
-		<button onClick={() => setIsOpen(true)}> Ouvrir le Panier</button>
+		<div className='lmj-cart-closed'>
+			<button
+				className='lmj-cart-toggle-button'
+				onClick={() => setIsOpen(true)}
+			>
+				Ouvrir le Panier
+			</button>
+		</div>
 	)
-
 }
 
 export default Cart
