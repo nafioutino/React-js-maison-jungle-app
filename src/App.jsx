8d5f72475c9c3ceecdc3be9186ec420ec6cfc255
nafioutino <nafiou531@gmail.com>
2024-12-30 10:59:09 +0100

    ajout des props

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-import Banner from './components/Banner'
-import logo from './assets/logo.png'
-import Cart from './components/Cart'
-import Footer from './components/Footer'
-import ShoppingList from './components/ShoppingList'
-import './styles/Layout.css'
+import Banner from "./components/Banner";
+import logo from "./assets/logo.png";
+import Cart from "./components/Cart";
+import Footer from "./components/Footer";
+import ShoppingList from "./components/ShoppingList";
+import "./styles/Layout.css";
+import { useState } from "react";
 
 function App() {
-	return (
-		<div>
-			<Banner>
-				<img src={logo} alt='La maison jungle' className='lmj-logo' />
-				<h1 className='lmj-title'>La maison jungle</h1>
-			</Banner>
-			<div className='lmj-layout-inner'>
-				<Cart />
-				<ShoppingList />
-			</div>
-			<Footer />
-		</div>
-	)
+  const [cart, updateCart] = useState([]);
+  return (
+    <div>
+      <Banner>
+        <img src={logo} alt="La maison jungle" className="lmj-logo" />
+        <h1 className="lmj-title">La maison jungle</h1>
+      </Banner>
+
+      <div className="lmj-layout-inner">
+        <Cart cart={cart} updateCart={updateCart} />
+        <ShoppingList cart={cart} updateCart={updateCart} />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/datas/plantList.jsx
+++ src/datas/plantList.jsx
@@ -1,13 +1,12 @@
 import monstera from '../assets/monstera.jpg'
-import basil from '../assets/basil.jpg'
-import cactus from '../assets/cactus.jpg'
-import calathea from '../assets/calathea.jpg'
 import lyrata from '../assets/lyrata.jpg'
-import mint from '../assets/mint.jpg'
-import olivier from '../assets/olivier.jpg'
 import pothos from '../assets/pothos.jpg'
 import succulent from '../assets/succulent.jpg'
-
+import olivier from '../assets/olivier.jpg'
+import basil from '../assets/basil.jpg'
+import mint from '../assets/mint.jpg'
+import calathea from '../assets/calathea.jpg'
+import cactus from '../assets/cactus.jpg'
 
 export const plantList = [
 	{
@@ -16,7 +15,8 @@ export const plantList = [
 		id: '1ed',
 		light: 2,
 		water: 3,
-		cover: monstera
+		cover: monstera,
+		price: 15
 	},
 	{
 		name: 'ficus lyrata',
@@ -24,23 +24,29 @@ export const plantList = [
 		id: '2ab',
 		light: 3,
 		water: 1,
-		cover: basil
+		cover: lyrata,
+		price: 16
 	},
+
 	{
 		name: 'pothos argenté',
 		category: 'classique',
 		id: '3sd',
 		light: 1,
 		water: 2,
-		cover: cactus
+		cover: pothos,
+
+		price: 9
 	},
 	{
-		name: 'yucca',
+		name: 'calathea',
 		category: 'classique',
 		id: '4kk',
-		light: 3,
-		water: 1,
-		cover: calathea
+		light: 2,
+		water: 3,
+		cover: calathea,
+
+		price: 20
 	},
 	{
 		name: 'olivier',
@@ -48,15 +54,18 @@ export const plantList = [
 		id: '5pl',
 		light: 3,
 		water: 1,
-		cover: lyrata
+		cover: olivier,
+		price: 25
 	},
+
 	{
-		name: 'géranium',
-		category: 'extérieur',
-		id: '6uo',
+		name: 'cactus',
+		category: 'plante grasse',
+		id: '8fp',
 		light: 2,
-		water: 2,
-		cover: mint
+		water: 1,
+		cover: cactus,
+		price: 6
 	},
 	{
 		name: 'basilique',
@@ -64,15 +73,8 @@ export const plantList = [
 		id: '7ie',
 		light: 2,
 		water: 3,
-		cover: olivier
-	},
-	{
-		name: 'aloe',
-		category: 'plante grasse',
-		id: '8fp',
-		light: 2,
-		water: 1,
-		cover: pothos
+		cover: basil,
+		price: 5
 	},
 	{
 		name: 'succulente',
@@ -80,6 +82,17 @@ export const plantList = [
 		id: '9vn',
 		light: 2,
 		water: 1,
-		cover: succulent
+		cover: succulent,
+		price: 8
+	},
+
+	{
+		name: 'menthe',
+		category: 'extérieur',
+		id: '6uo',
+		light: 2,
+		water: 2,
+		cover: mint,
+		price: 4
 	}
 ]
